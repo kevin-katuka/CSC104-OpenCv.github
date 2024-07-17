4457eccd301866bd24e184a7e01be99f70f917d5
kevin-katuka <149543488+kevin-katuka@users.noreply.github.com>
2024-07-17 13:03:21 +0000

    vid.py

---
+++ vid.py
@@ -0,0 +1,20 @@
+import cv2
+
+capture =cv2.VideoCapture(0)
+face_haar_cascade = cv2.CascadeClassifier('haarcascade frontalface_default.xml')
+while True:
+    _ , image = capture.read()
+    gray =cv2.cvtcolor(image , cv2 , COLOR_BGR2GRAY)
+    faces = face_haar_cascae.detectMultiScale(gray , 1.1 , 4 )
+
+
+
+    for (x , y , w , h ) in faces:
+        cv2.rectangle(image, (x , y ), (x+w , y+h )
+                         (0 , 255 , 0 ) , 5 )
+        cv2.imshow("video" , image )
+        k = cv2.waitkey(30 ) & 0xFF
+        if  k==27:
+            break
+
+        cv2.release()
