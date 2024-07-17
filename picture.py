4fdd561af0e76453c35ca75cfdd1011d51d35405
kevin-katuka <149543488+kevin-katuka@users.noreply.github.com>
2024-07-17 13:03:52 +0000

    picture.py

---
+++ picture.py
@@ -0,0 +1,15 @@
+import cv2
+
+face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+image= cv2.imread('face.jpeg')
+gray = cv2.cvtcolor(image, cv2 , COLOR_BGR2GRAY)
+#cv2.imshow('gray' , 'gray' )
+#cv2.waitKey()
+
+faces = face_haar_cascade.detectMultiScale (gray , 1.1, -4 )
+
+for (x , y , w , h ) in faces:
+    cv2.rectangle(image, (x , y ), (x+w , y+h )
+                     (0 , 255 , 0 ) , 5 )
+    cv2.imshow("faces" , image )
+    cv2.waitkey()
